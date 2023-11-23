511908c6a2f28c008ffc23560cb139b9b787dcb9
Sabeesmj786 <sabeesmj786@gmail.com>
2023-11-23 17:24:35 +0530

    commit work

---
+++ src/org/num/ForLoop.java
@@ -0,0 +1,15 @@
+package org.num;
+
+public class ForLoop {
+	
+	public static void main(String[] args) {
+		
+		for(int i=1;i>=100;i=i--) {
+			System.out.println(i);
+			}
+		}
+		
+	
+	
+	}
+
---
+++ src/org/num/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author msabe
+ *
+ */
+package org.num;
